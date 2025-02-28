05cbd0861d041d54f47a5181306a7243bbe8dd75
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-02-28 14:05:05 +0530

    remove bug

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ export function Navbar({}) {
     <nav className="flex mx-auto max-w-6xl items-center py-4 px-3 justify-between">
       <h3 className="text-xl text-white font-semibold">Emojify 👷</h3>
       <div className="flex gap-3 items-center">
-        <Button onClick={() => router.push("/")} variant="simple" className="px-2">home</Button>
+        <Button onClick={() => router.push("/#")} variant="simple" className="px-2">home</Button>
         <Button onClick={() => router.push("/#")} variant="simple" className="px-2">about</Button>
       </div>
     </nav>
